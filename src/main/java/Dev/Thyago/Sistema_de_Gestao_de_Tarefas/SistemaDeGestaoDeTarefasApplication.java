ac16e1944bd022da8defeec8d366a24e51b771f0
Thyago <thyagofranciscog@hotmail.com>
2026-05-12 18:08:04 -0300

    Primeiro commit

---
+++ src/main/java/Dev/Thyago/Sistema_de_Gestao_de_Tarefas/SistemaDeGestaoDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package Dev.Thyago.Sistema_de_Gestao_de_Tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestaoDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestaoDeTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Dev/Thyago/Sistema_de_Gestao_de_Tarefas/SistemaDeGestaoDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package Dev.Thyago.Sistema_de_Gestao_de_Tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestaoDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
